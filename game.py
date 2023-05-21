154f10fd112a30c9134112d13b51e99f8d9c8ad8
Urvil Patel <patelurvil38@gmail.com>
2023-05-20 21:36:57 -0400

    implemented the scoring logic and main game loop

--- game.py
+++ game.py
@@ -1,6 +1,5 @@
 import grid
 import sys
-import os
 from typing import Tuple
 
 banner = """
@@ -8,8 +7,7 @@ banner = """
 |     PEEK-A-BOO     |
 ----------------------
 """
-def clear():
-    os.system('clear')
+
 
 def manu():
     str = """
@@ -20,80 +18,125 @@ def manu():
 5. Exit"""
     print(str)
 
+
 def input_and_validate_coordinate(size) -> Tuple[int, int]:
     while True:
         co = input("Enter cell coordinates (e.g., a0): ").strip()
         if len(co) > 1:
             col = co[0].upper()
             row = co[1:]
-            if col.isalpha():
-                j = grid.alphabets.index(col)
-                if j > size-1:
-                    print("Input error: column entry is out of range for this grid. Please try again.")
-                    continue
-                elif j < 0:
-                    print("Invalid coordinate. Please try again.")
-                    continue
+
+            if not col.isalpha():
+                print("Error: Invalid coordinate. Please try again.")
+                continue
+
+            if not row.isdigit():
+                print("Error: Invalid coordinate. Please try again.")
+                continue
+
+            j = grid.alphabets.index(col)
+            if j > size-1:
+                print("Error: column entry is out of range for this grid. Please try again.")
+                continue
+            elif j < 0:
+                print("Error: Invalid coordinate. Please try again.")
+                continue
 
             try:
                 i = int(row)
                 if i > size-1:
-                    print("Input error: row entry is out of range for this grid. Please try again.")
+                    print("Error: row entry is out of range for this grid. Please try again.")
                     continue
             except:
-                print("Invalid coordinate. Please try again.")
+                print("Error: Invalid coordinate. Please try again.")
                 continue
-            
+
             return (i, j)
         else:
-            print("Invalid coordinate. Please try again.")
+            print("Error: Invalid coordinate. Please try again.")
 
 
-def choose_one_element(size) -> Tuple[int, int]:
-    return input_and_validate_coordinate(size)
-    
+class Game:
+    def __init__(self) -> None:
+        self.grid_size = self.get_grid_size()
+        self.use_emojis = self.should_use_emojis()
+        self.g = grid.Grid(self.grid_size, self.use_emojis)
 
-def game_loop():
-    print(banner)
-    g = grid.Grid(grid_size)
-    g.show()
-    while True:
-        manu()
-        try:
-            selected = int(input("\nSelect: "))
-            if selected == 1:
-                c1 = choose_one_element(g.size)
-                c2 = choose_one_element(g.size)
-                g.choose(g.index_at(c1[0], c1[1]), g.index_at(c2[0], c2[1]))
-            elif selected == 2:
-                co = choose_one_element(g.size)
-                g.faceup(g.index_at(co[0], co[1]))
-                clear()
-                g.show()
-            elif selected == 3:
-                clear()
-                g.show(reveal=True)
-            elif selected == 4:
-                g = grid.Grid(grid_size)
-                clear()
-                g.show()
-            elif selected == 5:
+    def get_grid_size(self):
+        if len(sys.argv) > 1:
+            try:
+                gsize = int(sys.argv[1])
+            except ValueError:
+                print("Error: Please provide valid grid size. must be 2, 4 or 6.")
+                exit(0)
+
+            if gsize not in [2, 4, 6]:
+                print("Error: Invalid grid size provided. must be 2, 4 or 6.")
+                exit(0)
+        else:
+            print("Error: Invalid execution.\nusage: python3 game.py <grid_size> [emojis]")
+            exit(0)
+        return gsize
+
+    def should_use_emojis(self):
+        if len(sys.argv) > 2:
+            if sys.argv[2] == "emojis":
+                return True
+        return False
+
+    def choose_one_element(self) -> Tuple[int, int]:
+        return input_and_validate_coordinate(self.grid_size)
+
+    def handle_option_1(self):
+        while True:
+            c1 = self.choose_one_element()
+            c2 = self.choose_one_element()
+            if c1 == c2:
+                print("Error: You've selected the same cells. Please select different cells.")
+                continue
+            else:
                 break
-        except ValueError:
-            print("Invalid Option")
+        self.g.choose(self.g.index_at(c1[0], c1[1]), self.g.index_at(c2[0], c2[1]))
+        return True
+
+    def handle_option_2(self):
+        co = self.choose_one_element()
+        self.g.faceup(self.g.index_at(co[0], co[1]))
+        grid.clear()
+        self.g.show()
+
+    def handle_option_3(self):
+        grid.clear()
+        self.g.show(reveal=True)
+
+    def handle_option_4(self):
+        self.g = grid.Grid(self.grid_size, self.use_emojis)
+        grid.clear()
+        self.g.show()
+
+    def loop(self):
+        grid.clear()
+        print(banner)
+        self.g.show()
+        while True:
+            manu()
+            try:
+                selected = int(input("\nSelect: "))
+                if selected == 1:
+                    if not self.handle_option_1():
+                        continue
+                elif selected == 2:
+                    self.handle_option_2()
+                elif selected == 3:
+                    self.handle_option_3()
+                elif selected == 4:
+                    self.handle_option_4()
+                elif selected == 5:
+                    break
+            except ValueError:
+                print("Invalid Option")
+
 
 if __name__ == "__main__":
-    grid_size = 2
-
-    if len(sys.argv) > 1:
-        try:
-            grid_size = int(sys.argv[1])
-        except ValueError:
-            print("Please provide valid grid size. must be 2, 4 or 6.")
-            exit(0)
-
-        if grid_size not in [2,4,6]:
-            print("Invalid grid size provided. must be 2, 4 or 6.")
-            exit(0)
-
-    game_loop()
+    game = Game()
+    game.loop()
--- grid.py
+++ grid.py
@@ -2,8 +2,14 @@ import random
 import copy
 import time
 import os
+import game
 
 alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+def clear():
+    os.system('clear')
+
 
 class Cell:
     def __init__(self, ident) -> None:
@@ -14,86 +20,112 @@ class Cell:
 
 
 class Grid:
-    def get_unique_identifier(self) -> int:
-        self.identifier += 1
-        return self.identifier
-    
-    def __init__(self, size) -> None:
+    def get_unique_identifier(self, use_emoji=False):
+        if use_emoji:
+            i = random.randrange(len(self.emojis))
+            return self.emojis.pop(i)
+        else:
+            print(len(self.numerics))
+            i = random.randrange(len(self.numerics))
+            return self.numerics.pop(i)
+
+    def __init__(self, size, use_emoji=False) -> None:
         self.identifier = 0
         self.grid = []
         self.size = size
+        self.emojis = [
+            "🚀", "🌍", "🌟", "🍕", "🎉", "🐼",
+            "🎸", "📚", "⚽", "🌺", "🌈", "🍔",
+            "🚲", "🌻", "🎈", "🏀", "🍦", "🌳"
+        ]
         self.number_of_pairs = (size * size) // 2
+        self.numerics = [str(i) for i in range(self.number_of_pairs)]
+        self.actual_guesses = 0
+        self.minimum_possible_guesses = self.number_of_pairs
         for _ in range(self.number_of_pairs):
-            cell = Cell(self.get_unique_identifier())
+            cell = Cell(self.get_unique_identifier(use_emoji))
             self.grid.append(copy.copy(cell))
             self.grid.append(copy.copy(cell))
         self.shuffle()
 
     def shuffle(self) -> None:
-        for i in range(len(self.grid)-1,0,-1):
+        for i in range(len(self.grid)-1, 0, -1):
             j = random.randint(0, i)
             self.grid[i], self.grid[j] = self.grid[j], self.grid[i]
 
     def at(self, i, j) -> Cell:
         return self.grid[(i*self.size)+j]
-    
+
     def index(self, i) -> Cell:
         return self.grid[i]
-    
-    # def get_one_and_only_faceup_cell_index(self):
-    #     found = None
-    #     for i in range(len(self.grid)):
-    #         if self.grid[i].is_faceup:
-    #             if found is None:
-    #                 found = i
-    #             else:
-    #                 return None
-    #     return found
-
-    # def set_one_and_only_faceup_cell_index(self, index):
-    #     for i in range(len(self.grid)):
-    #         self.grid[i].is_faceup = (i == index)
 
     def index_at(self, i, j) -> int:
         return (i*self.size)+j
-    
+
     def choose(self, i, j) -> None:
+        self.actual_guesses += 1
         c1 = self.grid[i]
         c2 = self.grid[j]
 
         c1.is_faceup = True
         c2.is_faceup = True
-        os.system('clear')
+        clear()
         self.show()
+        game.manu()
         if c1.ident == c2.ident:
             c1.is_matched = True
             c2.is_matched = True
         else:
-            c1.is_faceup = False
-            c2.is_faceup = False
+            if not c1.is_matched:
+                c1.is_faceup = False
+            if not c2.is_matched:
+                c2.is_faceup = False
             time.sleep(2)
-            os.system('clear')
-            self.show()
-           
+        clear()
+        self.show()
 
     def faceup(self, index) -> None:
-        self.grid[index].force_faceup = True 
+        self.actual_guesses += 2
+        self.grid[index].force_faceup = True
 
-    def show(self, reveal = False) -> None:
-        print()
-        print(" "*4, end='')
+    def show(self, reveal=False) -> None:
+        padding = 7
+        print("\n"+" "*4, end='')
         for i in range(self.size):
-            print("["+alphabets[i]+"]  ", end='')
+            print(("["+alphabets[i]+"]").center(padding," "), end='')
         print('\n')
-
 
         for i in range(self.size):
             print("["+str(i)+"] ", end='')
             for j in range(self.size):
                 cell = self.at(i, j)
                 if cell.is_faceup or cell.is_matched or reveal or cell.force_faceup:
-                    print(str(cell.ident).rjust(2)+"   ",end='')
+                    if cell.ident.isdigit():
+                        print(str(cell.ident).center(padding, " "), end='')
+                    else:
+                        print(str(cell.ident).center(padding-1, " "), end='')
                 else:
-                    print("X".rjust(2)+"   ", end='')
-            
+                    print("X".center(padding, " "), end='')
+
             print('\n')
+
+        if self.game_over():
+            score = (self.minimum_possible_guesses / self.actual_guesses) * 100
+            if self.cheated():
+                print("You cheated - Loser!. You're score is 0!")
+            else:
+                print("Oh Happy Day. You've won!! Your score is: {:.2f}".format(score))
+
+    def game_over(self):
+        revealed_cell = 0
+        for c in self.grid:
+            if c.is_faceup or c.force_faceup:
+                revealed_cell += 1
+        return revealed_cell == len(self.grid)
+
+    def cheated(self):
+        revealed_cell = 0
+        for c in self.grid:
+            if c.force_faceup:
+                revealed_cell += 1
+        return revealed_cell == len(self.grid)
